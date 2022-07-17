fb85e82f2a37d5de646b5eb3f707f490f1980419
andyfree96 <anthonyfree96@gmail.com>
2022-07-17 21:54:47 +0800

    vue-router

---
+++ vue-router/src/views/ProfileView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Profile Info</h1>
+    <p>username : {{ this.$route.params.username }}</p>
+    <p>id : {{ this.$route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
+
+<style scoped></style>
