5ca06f0706fdff80ecf98054b996dafe3222e78d
andyfree96 <anthonyfree96@gmail.com>
2022-07-19 15:32:34 +0800

    covid-19 add numberWithCommas filter

--- covid-19/src/components/DataBox.vue
+++ covid-19/src/components/DataBox.vue
@@ -2,13 +2,13 @@
   <div class="data-box-container">
     <div class="confirm">
       <h4>Confirm</h4>
-      New: <span>{{ stats.NewConfirmed }}</span> Total:
-      <span>{{ stats.TotalConfirmed }}</span>
+      New: <span>{{ stats.NewConfirmed | numberWithCommas }}</span> Total:
+      <span>{{ stats.TotalConfirmed | numberWithCommas }}</span>
     </div>
     <div class="death">
       <h4>Death</h4>
-      New: <span>{{ stats.NewDeaths }}</span> Total:
-      <span>{{ stats.TotalDeaths }}</span>
+      New: <span>{{ stats.NewDeaths | numberWithCommas }}</span> Total:
+      <span>{{ stats.TotalDeaths | numberWithCommas }}</span>
     </div>
   </div>
 </template>
--- covid-19/src/main.js
+++ covid-19/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter("numberWithCommas", (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
